324abde66ba832456d8c224dab619c4e505f9f14
shailendraai <115892306+shailendraai@users.noreply.github.com>
2022-10-16 12:29:54 +0530

    Create class.cpp

---
+++ class.cpp
@@ -0,0 +1,7 @@
+class:
+      class is user-defined data type that we use in our programs.
+object: 
+       object is instance/element of class which inherits all the variables and functions
+       from class.
+       To access data members and member functions defined in the class, we need to create objects. like obj.brand etc.
+Exapmle:
